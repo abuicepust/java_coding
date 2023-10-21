02a53989ed216dc739973bf62f3d5b9e386b08ed
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-21 12:20:12 +0600

    Add files via upload
    difference between static and dynamic concept.

---
+++ Java_Programming/static_.java
@@ -0,0 +1,17 @@
+public class static_ {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello this is function");
+        //abu f1 = new abu();
+        sum(11,9);
+    }
+    static void sum(int a, int b)
+    {
+        int num1,num2,s;
+        num1 = a;
+        num2 = b;
+        s = num1 + num2;
+        System.out.println("The sum is: "+s);
+        
+    }
+}
