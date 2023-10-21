bc29be32e4b66851bd43985f0fdfabd8ed6f56ec
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-21 21:20:17 +0600

    Add files via upload
    How we can execute function inside the same class.

---
+++ Java_Programming/function_same_class.java
@@ -0,0 +1,11 @@
+public class function_same_class {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        fun();
+    }
+    static void fun()
+    {
+        System.out.println("This is inside the function");
+    }
+}
