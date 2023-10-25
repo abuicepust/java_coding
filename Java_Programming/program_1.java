ae8cbc571542d712c2dd6333d2ddc56f2f7be3e3
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-26 00:36:58 +0600

    Add files via upload

---
+++ Java_Programming/program_1.java
@@ -0,0 +1,12 @@
+public class program_1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+        int arr[] = {1,2,3,4,5};
+        for(int i=0;i<arr.length-2;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
