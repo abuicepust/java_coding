67dd31c0af4bb5029b89548969d4e30d2886f1f4
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-25 18:16:21 +0600

    Add files via upload
    While loop using java

---
+++ Java_Programming/While1.java
@@ -0,0 +1,13 @@
+public class While1{
+    public static void main(String[] args){
+        System.out.println("This is while loop");
+
+        int n =0;
+        while(n <= 9)
+        {
+            System.out.println(n);
+            n++;
+        }
+        System.out.println(n);
+    }
+}
