7fbb7a1452a1f8d5b130cf8bbf4ba06eae0a0669
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-26 12:00:37 +0600

    Add files via upload
    Using break statement in java.

---
+++ Java_Programming/break1.java
@@ -0,0 +1,16 @@
+public class break1 {
+    public static void main(String[] args){
+        System.out.println("Abu");
+
+        int n = 10;
+        switch(n){
+            case 23:
+            {
+                System.out.println("He is adult");
+                break;
+            }
+            default:
+                System.out.println("Not an adult");
+        }
+    }
+}
