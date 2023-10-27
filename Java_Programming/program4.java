4e81f4fc68a908f55aa0a3d72bb120cb1fb97a7c
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-27 17:27:27 +0600

    Add files via upload
    Pre-increment and post-increment working principles

---
+++ Java_Programming/program4.java
@@ -0,0 +1,10 @@
+public class program4 {
+    public static void main(String[] args){
+        int x=10;
+        int y=10;
+        int w = ++x;
+        int z = y++;
+        System.out.println(w+" "+z);
+        System.out.println(++y);
+    }
+}
