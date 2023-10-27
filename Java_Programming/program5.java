80437145c7998939912d13696794b5e1f7f833f4
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-28 00:35:47 +0600

    Add files via upload
    Pre-decrement and Post decrement

---
+++ Java_Programming/program5.java
@@ -0,0 +1,15 @@
+public class program5 {
+    public static void main(String[] args)
+    {
+        System.out.println("Increment and Decrement: ");
+        int w = 10;
+        int x = 10;
+        int y = --w;
+        int z = x--;
+        System.out.println("This is the value of y " +y);
+        System.out.println("This is the value of z " +z);
+        //value of x
+        System.out.println(x);
+
+    }
+}
