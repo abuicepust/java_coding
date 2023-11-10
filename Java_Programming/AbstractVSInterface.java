c2565aca052f0a6f9687b790514f31f91aa64398
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-10 23:49:04 +0600

    Add files via upload
    Comparison between them.

---
+++ Java_Programming/AbstractVSInterface.java
@@ -0,0 +1,22 @@
+abstract class Hello1{
+    //abstract public void run23();
+}
+abstract class Hello2{
+    //abstract public void run23();
+}
+interface Mobile1{
+    //public void run24();
+}
+interface Mobile2{
+    //public void run24();
+}
+class childclass implements Mobile1,Mobile2{
+
+}
+
+public class AbstractVSInterface {
+    public static void main(String[] args) {
+        System.out.println("This is abstract vs interface exaple");
+
+    }
+}
