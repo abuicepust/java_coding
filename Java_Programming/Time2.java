58265224276f1e3490156324ba4328778a298e37
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-21 20:35:53 +0600

    Add files via upload
    How time works in java.

---
+++ Java_Programming/Time2.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Time2 {
+    public static void main(String[] args) {
+        System.out.println("Time in java");
+
+        LocalTime obj = LocalTime.now();
+        System.out.println(obj);
+    }
+}
