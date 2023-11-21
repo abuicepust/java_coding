ae65fed92431b139e20bfe0be954101e7c2028ba
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-21 20:33:41 +0600

    Add files via upload
    How can we see date in java.

---
+++ Java_Programming/Time1.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Time1 {
+    public static void main(String[] args) {
+        System.out.println("Example of time");
+
+        LocalDate obj = LocalDate.now();
+        System.out.println(obj);
+    }
+}
