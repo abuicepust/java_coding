cbf118eaaf5f2c1249607aa41692257fe3da48e2
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-21 20:39:08 +0600

    Add files via upload
    Allow us to show both date and time.

---
+++ Java_Programming/Time3.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Time3 {
+    public static void main(String[] args) {
+        System.out.println("Local Time and Date");
+
+        LocalDateTime obj = LocalDateTime.now();
+        System.out.println(obj);
+    }
+}
