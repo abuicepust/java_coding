33ff95d530648fd8f3eb0695f8bb4b872bc27656
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-22 20:46:33 +0600

    Add files via upload
    Arralist in java

---
+++ Java_Programming/ArrayList1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        
+        ArrayList<String> cars = new ArrayList<>();
+
+        cars.add("BMW");
+        cars.add("Farrari");
+        cars.add("Tesla");
+        cars.add("Poor no car");
+
+
+        System.out.println(cars);
+
+        
+    }
+}
