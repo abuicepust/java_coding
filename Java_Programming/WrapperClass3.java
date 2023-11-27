1e3fb5444e20cfdb8ec4c3fc6aca7da83b42f8c2
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-11-27 15:16:21 +0600

    Add files via upload
    Wrapper Classes and It's working principles.

---
+++ Java_Programming/WrapperClass3.java
@@ -0,0 +1,8 @@
+public class WrapperClass3 {
+        public static void main(String[] args) {
+          Integer myInt = 100;
+          String myString = myInt.toString();
+          System.out.println(myString);
+          System.out.println(myString.length());
+        }
+}
