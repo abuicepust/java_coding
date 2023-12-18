49378a7b5d96045f7ec4467af4bcc991888611ab
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-18 20:45:41 +0600

    Add files via upload

---
+++ Java_Programming/ReplaceAll.java
@@ -0,0 +1,9 @@
+public class ReplaceAll {
+    public static void main(String[] args) {
+        String a = "I am Abu Yousuf Sojib";
+
+        a = a.replaceAll(" ", "Space");
+
+        System.out.println(a);
+    }
+}
